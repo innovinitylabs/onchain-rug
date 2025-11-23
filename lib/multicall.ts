c1b62a0786a45458da6480a569f617984da93ebe
innovinitylabs <rajasekarcmr@icloud.com>
2025-11-23 17:34:59 +0530

    FIX: Add missing getNetworkByChainId import
    - Function was being used but not imported
    - Added the import to fix the build

--- lib/multicall.ts
+++ lib/multicall.ts
@@ -1,5 +1,5 @@
 import { createPublicClient, http } from 'viem'
-import { getNetworkByChainId } from './networks'
+import { getNetworkByChainId, getRpcUrl } from './networks'
 
 /**
  * Create a chain client for blockchain interactions
