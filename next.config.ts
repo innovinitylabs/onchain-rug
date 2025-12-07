82b25be813755d4528e6cf6359d3aabbad5d084b
innovinitylabs <rajasekarcmr@icloud.com>
2025-12-07 17:13:25 +0530

    Exclude murky library scripts from Next.js build
    - Add webpack IgnorePlugin to exclude script files that use Node.js-specific modules
    - Add scripts directory to tsconfig exclude list
    - Fixes build error: Cannot find module 'ethereumjs-util'

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Exclude smart contract directories from Next.js build
   // See .nextignore for detailed exclusions
+  webpack: (config) => {
+    // Ignore script files from murky library that use Node.js-specific modules
+    const webpack = require('webpack');
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /\/lib\/creator-token-contracts\/lib\/murky\/.*\/scripts\/.*$/,
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
